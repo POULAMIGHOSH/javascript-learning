4f424a73e4d37af3b3c93bb111bbe29351aa40d9
POULAMIGHOSH <poulami.21ghosh@gmail.com>
2023-01-19 23:40:40 +0530

    mark john bmi calculator

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-alert("are you okay?");
+// alert("are you okay?");
 
-const name_p = "joy";
-console.log(name_p);
+// const name_p = "joy";
+// console.log(name_p);
+const markWeightArray = [78, 95];
+const johnWeightArray = [92, 85];
+const markHeightArray = [1.69, 1.88];
+const johnHeightArray = [1.95, 1.76];
+// const markWeight = 78;
+// const johnWeight = 92;
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+
+for (let i = 0; i < 2; i++) {
+  const markBMI = markWeightArray[i] / markHeightArray[i] ** 2;
+  const johmBMI = johnWeightArray[i] / johnHeightArray[i] ** 2;
+
+  const markHigherBMI = markBMI > johmBMI;
+
+  console.log(markHigherBMI, "markHigherBMI");
+}
